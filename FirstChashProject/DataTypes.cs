58735b90bbb843b0d169979b5d452d1d3c5ab1e5
GowherAhanger <gowherrashid22@gmail.com>
2026-01-15 16:43:44 +0530

    Added Datatype program amd called it in main program

---
+++ FirstChashProject/DataTypes.cs
@@ -0,0 +1,20 @@
+namespace FirstChashProject;
+using System;
+
+public class DataTypes
+{
+    
+    private int a = 400;
+    
+    private long b = 20000000000;
+    float c = 3.14159f;
+    double d = 3.14159d;
+
+    public  void PrintDataTypes()
+    {
+        Console.WriteLine(a);
+        Console.WriteLine(b);
+        Console.WriteLine(c);
+        Console.WriteLine(d);
+    }
+}
