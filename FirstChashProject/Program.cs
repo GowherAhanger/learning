97a23565d6574ab9d5bb21273289d7538e8a72bb
GowherAhanger <gowherrashid22@gmail.com>
2026-01-16 15:01:09 +0530

    Implicit casting program added objects for implicit casting program in main class "display and print"

--- FirstChashProject/Program.cs
+++ FirstChashProject/Program.cs
@@ -5,8 +5,15 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Enter Value ");
-        string value = Console.ReadLine();
-        Console.WriteLine("Entered  " + value);
+       Console.WriteLine("Enter Value ");
+       string value = Console.ReadLine();
+       Console.WriteLine("Entered  " + value);
+        DataTypes data = new DataTypes();
+        data.PrintDataTypes();
+        //obj of implecit TypeCasting........
+        
+        TypeCasting obj = new TypeCasting();
+        obj.display();
+        obj.print();
     }
 }
